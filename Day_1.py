4fbeec89ab4357901873f4686142f1442f6356a7
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-02-18 22:08:01 +0530

    initial commit

---
+++ Day_1.py
@@ -0,0 +1,7 @@
+#project name: Band name generator
+# used : Inbuilt functions , variables and string concatenation 
+
+print("welcome to the band name generator")
+city = input("what is the city you grew up in?\n")
+pet = input("what is your pet name?\n")
+print("your band name could be : "+city+" "+pet)
