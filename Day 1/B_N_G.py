622b6e6430654a28a3de86b506de59b7a9946579
Suhas Gowda HN <suhasgowda.aiml@gmail.com>
2026-02-27 22:31:17 +0530

    My first python project
    Added a comment encouraging users to follow along for a fun learning experience.

--- Day 1/B_N_G.py
+++ Day 1/B_N_G.py
@@ -5,3 +5,5 @@ print("welcome to the band name generator")
 city = input("what is the city you grew up in?\n")
 pet = input("what is your pet name?\n")
 print("your band name could be : "+city+" "+pet)
+
+#you can modify according to your needs but if you are looking this , trust me follow this until the end you will learn this with fun
